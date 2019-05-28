79fb5f9f4bfe724e5560fb6feb2c787493b51844
92477 <92477@DESKTOP-V2ABVJK>
2019-05-28 16:49:56 +0800

    测试类

--- test/com/workblog/zjy/TestTagDao.java
+++ test/com/workblog/zjy/TestTagDao.java
@@ -1,8 +1,28 @@
 package com.workblog.zjy;
 
+import java.util.Date;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringT)
+import com.sunshareteam.workblog.dao.TagMapper;
+import com.sunshareteam.workblog.entity.Tag;
+
+@RunWith(SpringRunner.class)
 public class TestTagDao {
-
+	@Autowired
+	private TagMapper tagMapper;
+	
+	@Test
+	public void test1() {
+		Tag tag=new Tag();
+		tag.setTagtitle("1111");
+		tag.setCreatedate(new Date());
+		tag.setCreateuser(new Integer(1));
+		tag.setModifydate(new Date());
+        tag.setModifyuser(new Integer(1));
+		tagMapper.addTag(tag);
+	}
 }
