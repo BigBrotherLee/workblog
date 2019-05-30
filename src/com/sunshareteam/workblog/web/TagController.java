998be97c67b845321ff007f5ed63aae824d775b6
92477 <92477@DESKTOP-V2ABVJK>
2019-05-30 10:39:29 +0800

    标签接口

--- src/com/sunshareteam/workblog/web/TagController.java
+++ src/com/sunshareteam/workblog/web/TagController.java
@@ -1,13 +1,88 @@
 package com.sunshareteam.workblog.web;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bigbrotherlee.utils.ResponseResult;
+import com.sunshareteam.workblog.entity.Tag;
 import com.sunshareteam.workblog.service.TagService;
 
 @RestController("/tag")
 public class TagController {
 //	@Autowired
 	private TagService tagService;
-
 	
+	
+	/** 
+	 * 删除标签，只有角色中管理员才可以可以调用
+	 * @param tagid
+	 * @return 删除成功则返回ResponseResult<String>，state:1,message禁用失败，失败抛出异常
+	 */
+	@RequiresRoles("admin")
+	@RequiresPermissions("tag:delete:*")
+	@DeleteMapping("/delete/{tagid}")
+       public ResponseResult<String> deleteTag(@PathVariable Integer tagid){
+    	
+    	ResponseResult<String> result=new ResponseResult<String>();
+    	
+    	return result;
+		
+	}
+	  /** 
+	   * 更改标签信息
+	   * @param tag 更改后的标签信息
+	   * @param session
+	   * @return 更改成功则返回ResponseResult<Tag>,state:1,message:修改成功,date:更改后的标签信息json，失败抛出异常
+	   */
+	 @RequiresRoles("admin")
+	 @RequiresPermissions("tag:update:*") 
+	 @PutMapping("/update") 
+	    public ResponseResult<Tag> updateTag(Tag tag,HttpSession session){
+		   ResponseResult<Tag> result=new ResponseResult<Tag>();
+		   Tag relTag=(Tag) SecurityUtils.getSubject().getPrincipals();
+		   SecurityUtils.getSubject().hasRole("admin");
+		   return result;
+	 }
+	 /**
+	  * 创建标签，创建一个全新的标签
+	  * @param tag 你要创建的标签信息
+	  * @return 更改成功则返回ResponseResult<Tag>,state:1,message:创建成功,date:创建后的标签信息json，失败抛出异常
+	  */
+	 @RequiresPermissions("tag:insert:*")
+	 @PostMapping("/add") 
+	    public ResponseResult<Tag> addTag(Tag tag){
+		 ResponseResult<Tag> result=new ResponseResult<Tag>();
+		 return result;
+     }
+	 /** 
+	  * 查找标签
+	 * @param tagid 你要查找的标签Id
+	 * @return 查找成功则返回ResponseResult<String>，state:1,message禁用失败，失败抛出异常
+	 */
+	 @GetMapping("/fing/{tagid}") 
+	    public ResponseResult<String> findTadId(@PathVariable Integer tagid){
+		 ResponseResult<String> result=new ResponseResult<String>();
+		 return result;
+     }
+	 
+	 /**
+	  * 添加已有标签，添加标签id和文章id
+	 * @param tag 
+	 * @return 添加成功则返回ResponseResult<String>，state:1,message禁用失败，失败抛出异常
+	 */
+	 @RequiresPermissions("tag:insert:*")
+	 @PostMapping("/establish") 
+	    public ResponseResult<String> establishTag(Integer articleid,Integer tagid){
+		ResponseResult<String> result=new ResponseResult<String>();
+		return result;
+     } 
 }
