bb9e4ef6cd6d6751d5b1e8a9e5eecdd57eb93d1c
lee <lee@lee>
2019-06-04 14:50:10 +0800

    message

--- test/com/workblog/lsy/TestMapper.java
+++ test/com/workblog/lsy/TestMapper.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +34,7 @@ public class TestMapper {
 		user.setAutograph("你好，世界人");
 		user.setEmail("123@qq.com");
 		String salt=Integer.toString(RandomUtils.nextInt(1000, 9999));
-		String pwd=new SimpleHash("Md5", "123456",salt.getBytes(), 2).toString();
+		String pwd=new SimpleHash("Md5", "123456",ByteSource.Util.bytes(salt), 2).toString();
 		user.setPassword(pwd);
 		user.setPhone("1786132477");
 		user.setPic("ghasdjhfg");
